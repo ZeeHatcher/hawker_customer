4f476492d9ffd902e8abdd603fe598da85da49c7
Jonathan Seng <zee.hatcher010@gmail.com>
2020-12-11 16:25:09 +0800

    add notification

--- app/src/main/java/com/example/hawker_customer/MainActivity.java
+++ app/src/main/java/com/example/hawker_customer/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.hawker_customer;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,16 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity implements NavigationHost, ItemsAdapter.OnItemClickListener {
 
+    public static final String CHANNEL_ID = "com.example.hawker_vendor.NOTIFICATION";
+
     private static String TAG = "Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
 
         navigateTo(LoginFragment.newInstance(), false);
     }
@@ -40,5 +47,18 @@ public class MainActivity extends AppCompatActivity implements NavigationHost, I
     public void onItemClick(Item item, Customer customer) {
         QuantityDialogFragment dialog = new QuantityDialogFragment(item, customer);
         dialog.show(getSupportFragmentManager(), "quantity");
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
